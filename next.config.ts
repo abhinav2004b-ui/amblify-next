9a0e172ae9068faa0f807d891ff8d6bca28a672e
abhinav2004b-ui <abhinav2004b@gmail.com>
2026-02-10 14:48:52 +0530

    Migrate rewrite rules to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/cms/:path*',
+        destination: 'http://147.93.78.205/cms/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
